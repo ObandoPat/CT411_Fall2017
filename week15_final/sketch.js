1884402ecc17872ae4372865e7895c5489a51ac2
ObandoPat <31457908+ObandoPat@users.noreply.github.com>
2017-11-28 21:23:54 -0500

    final checkin 2

--- week15_final/sketch.js
+++ week15_final/sketch.js
@@ -1,295 +1,132 @@
-function Bloon() {
-// variables
-    this.x = random(0,width),
-    this.y = random(-700,0),
-    this.speed = random(0,10),
-    this.circDiameter = 50,
-    this.circRadius = this.circDiameter/2,
-    this.d,
-        
-// functions
-    this.drawBall = function(){
-        ellipse(this.x, this.y, this.circDiameter, this.circDiameter);
-        },
-    this.bounce = function(){
-        if(this.y < height){
-            this.speed = this.speed + 0.1;
-            }
-        },
-    this.moveBall = function(){
-        this.y = this.y + this.speed;
-        }
-    this.noDodge = function(){
-        this.d = dist(this.x, this.y, mouseX, mouseY);
+// constructor
+    function Bloon() {
     
-        if(this.d < this.circRadius){
-            background(0, 200);
-            noLoop();
-            score = score + 1;
-            text("Try again?", width/2, height/2, 300, 800);
+    // variables
+        this.x = random(0,width),
+        this.y = random(-5000,-1000),
+        this.speed = 10,
+        this.circDiameter = 50,
+        this.circRadius = this.circDiameter/2,
+        this.d,
+        this.d = dist(this.x, this.y, mouseX, mouseY),
+
+    // functions
+        this.drawBall = function(){
+            ellipse(this.x, this.y, this.circDiameter, this.circDiameter);
+            },
+        this.bounce = function(){
+            if(this.y < height){
+                this.speed = this.speed + 0.1;
+                }
+},
+        this.moveBall = function(){
+            this.y = this.y + this.speed;
+}
+        this.noDodge = function(){
+            this.d = dist(this.x, this.y, mouseX, mouseY);
+
+            if(this.d < this.circRadius){
+                background(150, 159, 85);
+//                score = score + 1;
+                noLoop();
+                    fill(255);
+                    textSize(40);
+                text("Click here to try again", width/3, height -100, 400, 800);
+                
+                }
             }
-        }
-}
-
-function rethrow(){
-    clear();
-    background(250, 190, 175); 
-        Bloon.x = random(0,width);
-        Bloon.y = random(-700,0);
-//    throwBalls();
-    loop();
-}
-
-
-// array
-    var bloons = [];
-//    var isClicked = false;
-    var score = 0;
-
-// buttons
-        var rect1 = {
-            x: 100,
-            y: 100,
-            size: 100,
-        //    top: rect1.y,
-        //    bottom: rect1.y + rect1.size,
-        //    left: rect1.x,
-        //    right: rect1.x + rect1.size
-        };
-
-        rectTop = rect1.y;
-        rectBottom = rect1.y + rect1.size;
-        rectLeft = rect1.x;
-        rectRight = rect1.x + rect1.size;
+    }
 
 // functions
     function scoreBoard() {
+            textAlign(LEFT);
+            textSize(20);
+            fill(256);
+        text("NUMBER OF ATTEMPTS", 50, 50, 200, 500);
+            textAlign(CENTER);
             textSize(40);
             fill(256);
-        text(score, 50, 50, 150, 500);     
-}
+        text(score, 25, 100, 150, 500);
+};
+
+    function gameStart() {
+        textSize(100);
+        fill(253, 231, 225, random(0,255));
+    text(areReady, width/5, 300, 800, 500);
+};
 
     function throwBalls(){
         fill(150, 159, 85);
         noStroke();
-    for (var i=0; i < bloons.length; i++){
-        bloons[i].drawBall();
-        bloons[i].bounce();
-        bloons[i].moveBall();
-        bloons[i].noDodge();
-    };
+        for (var i=0; i < bloons.length; i++){
+            bloons[i].drawBall();
+            bloons[i].bounce();
+            bloons[i].moveBall();
+            bloons[i].noDodge();
+        };
 
     scoreBoard();
-}
+};
 
-    function setup(){
-        createCanvas(windowWidth, windowHeight);
-        for (var i = 0; i < random(50,100); i++){
+    function rethrow(){
+//        background(250, 190, 175);
+//        text("Dodge All The Balls", width/5, 200, 800, 500);
+//        
+        for (var i = 0; i < random(200,500); i++){
             bloons[i] = new Bloon();
         }
+        loop();
+};
+
+// array
+    var bloons = [];
+    var score = 0;
+
+
+// buttons
+    var rect1 = {
+            x: 100,
+            y: 100,
+            size: 100,
+        };
+
+    rectTop = rect1.y;
+    rectBottom = rect1.y + rect1.size;
+    rectLeft = rect1.x;
+    rectRight = rect1.x + rect1.size;
+
+var areReady = "Are you ready? Click to start";
+
+
+function preload(){
+    rumbleSound = loadSound("rumble.mp3");
 }
 
+
+function setup(){
+    createCanvas(windowWidth, windowHeight);
+    rumbleSound.loop();
+};
 
 
 function draw(){
-    background(250, 190, 175);
-    setInterval(throwBalls(), 5000);
-    
-    fill(150, 10, 100);
-    rect(rect1.x, rect1.y, rect1.size, rect1.size);
+    background(250, 190, 175);   
+    throwBalls();
+    gameStart();  
 }
-
 
 
 function mousePressed(){
-
-    rethrow();    
-    
-//    if(mouseX > rectLeft && mouseX < rectRight && mouseY > rectTop && mouseY < rectBottom && isClicked == false){
-//        clear();
-//        background(250, 190, 175); 
-//        loop();
-//            Bloon.x = random(0,width);
-//            Bloon.y = random(-700,0);
-//        throwBalls();
-//    }
+//        textSize(100);
+    areReady = "Dodge all the incoming balls";
+    rethrow();  
+    score = score + 1;
 }
 
 
 
-
-
-
-////    remove();
-//
-//    if(mouseX > rectLeft && mouseX < rectRight && mouseY > rectTop && mouseY < rectBottom){
-//        clear();
-//        background(250, 190, 175);
-//        throwBalls();
-//    }
-//}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//var rect1 = {
-//    x: 100,
-//    y: 100,
-//    size: 100,
-//};
-//
-//rectTop = rect1.y;
-//rectBottom = rect1.y + rect1.size;
-//rectLeft = rect1.x;
-//rectRight = rect1.x + rect1.size;
-//
-//var colR;
-//
-//function setup() {
-//    createCanvas(windowWidth, windowHeight);
-//    
-//}
-//
-//function draw() {
-//    background(200);
-//
-//    fill(colR, 10, 100);
-//    rect(rect1.x, rect1.y, rect1.size, rect1.size);
-//    
-//    if(mouseX > rectLeft && mouseX < rectRight && mouseY > rectTop && mouseY < rectBottom){
-//        colR = 0;    
-//    } else{
-//        colR = 255;  
-//    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//function Bloon() {
-//// variables
-//    this.x = random(0,width),
-//    this.y = random(-700,0),
-//    this.speed = random(0,10),
-//    this.circDiameter = 50,
-//    this.circRadius = this.circDiameter/2,
-//    this.d,
-//
-//        
-//// functions
-//    this.drawBall = function(){
-//        ellipse(this.x, this.y, this.circDiameter, this.circDiameter);
-//        },
-//    this.bounce = function(){
-//        if(this.y < height){
-//            this.speed = this.speed + 0.1;
-//            }
-//        },
-//    this.moveBall = function(){
-//        this.y = this.y + this.speed;
-//        }
-//    
-//    this.noDodge = function(){
-//        this.d = dist(this.x, this.y, mouseX, mouseY);
-//    
-//        if(this.d < this.circRadius){
-//            background(0);
-//            noLoop();
-//            score = score + 1;
-//            text("Try again?", width/2, height/2, 300, 800);
-//        }    
-//    }
-//
-//function scoreBoard() {
-//        textSize(40);
-//        fill(256);
-//    text(score, 50, 50, 150, 500);     
-//}
-//
-//// array
-//    var bloons = [];
-//    var isClicked = false;
-//    var score = 0;
-//    
-//// functions
-//function throwBalls(){
-//        fill(150, 159, 85);
-//        noStroke();
-//    for (var i=0; i < bloons.length; i++){
-//        bloons[i].drawBall();
-//        bloons[i].bounce();
-//        bloons[i].moveBall();
-//        bloons[i].noDodge();
-//    };
-//}
-//
-//
-//    scoreBoard();
-//    }
-//    
-////function reThrow(){
-////        background(250, 190, 175);
-////        throwBalls();
-////    }  
-//
-//
-//    
-//    
-//    
-//    
-//    
-//function setup(){
-//    createCanvas(windowWidth, windowHeight);
-//    for (var i = 0; i < random(50,100); i++){
-//        bloons[i] = new Bloon();
-//    }
-//}
-//
-//
-//
-//
-//function draw(){
-//    background(250, 190, 175);
+//        textSize(150);
+//        fill(192, 175, 171);
+//    text("Dodge All The Balls", width/5 + 3, 200 + 5, 800, 500);
 //    throwBalls();
-//}
-//    
-//function mousePressed(){
-//    if(mouseX > restartButtonLeft && mouseX < restartButtonRight && mouseY > restartButtonTop && mouseY < restartButtonBottom){
-////        loop();
-//        background(250, 190, 175);
-//        function draw(){
-//    background(250, 190, 175);
-////    redraw();
-//    throwBalls();
-//}
-//    }
-//}
+
